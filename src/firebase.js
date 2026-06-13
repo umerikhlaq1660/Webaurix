1192d31851774a753549e19ee6978f1c1c64872b
Exofe <info.exofe@gmail.com>
2026-06-13 13:43:29 +0500

    Hardcode Firebase config for Cloudflare Worker deployment

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyApxIf6OhQn18p_oRH5TM8QxYH2t3Wsmmw",
+  authDomain: "webaurixsite.firebaseapp.com",
+  projectId: "webaurixsite",
+  storageBucket: "webaurixsite.appspot.com",
+  messagingSenderId: "1057936914192",
+  appId: "1:1057936914192:web:2ffba7c6b4f0b58b909795",
+  measurementId: "G-LGHYQCWFQD",
 };
 
 const app = initializeApp(firebaseConfig);
